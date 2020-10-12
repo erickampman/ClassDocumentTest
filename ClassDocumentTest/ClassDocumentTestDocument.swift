4dee4b2084bfeb9de3556bfa6033d0f56cbffbeb
Eric Kampman <erickampman@yahoo.com>
2020-10-12 06:01:34 -0700

    doc type to class

--- ClassDocumentTest/ClassDocumentTestDocument.swift
+++ ClassDocumentTest/ClassDocumentTestDocument.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 import UniformTypeIdentifiers
 
 extension UTType {
@@ -14,26 +15,37 @@ extension UTType {
     }
 }
 
-struct ClassDocumentTestDocument: FileDocument {
-    var text: String
-
+class ClassDocumentTestDocument: FileDocument, ObservableObject {
+   @Published var text: String
+	var subscriber: AnyCancellable!
+	
     init(text: String = "Hello, world!") {
         self.text = text
+		
+		setupSink()
     }
 
     static var readableContentTypes: [UTType] { [.exampleText] }
 
-    init(configuration: ReadConfiguration) throws {
+	required init(configuration: ReadConfiguration) throws {
         guard let data = configuration.file.regularFileContents,
               let string = String(data: data, encoding: .utf8)
         else {
             throw CocoaError(.fileReadCorruptFile)
         }
         text = string
+		setupSink()
     }
     
     func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
         let data = text.data(using: .utf8)!
         return .init(regularFileWithContents: data)
     }
+	
+	func setupSink() {
+		subscriber = $text.sink(receiveCompletion: { print("Completion: \($0)") },
+									receiveValue: { print("Data: \($0)") } )
+	}
+
+
 }
